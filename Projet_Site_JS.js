a256caa98375dd77739a844317217c6a0acb94f6
Anais <anais.hammouche@edu.itescia.fr>
2020-11-04 15:00:05 +0100

    home page + serie 2 new

---
+++ Projet_Site_JS.js
@@ -0,0 +1,21 @@
+let burger = "menu"; // variable
+
+let un = "Accueil";
+document.write(un)
+
+let deux = "Catégories";
+document.write(deux);
+document.querySelector('deux').addEventListener('click', function() 
+{
+    alert('Aïe, arrêtez de cliquer !!')});
+
+
+// alert("Site en cours de construction"); // fontion ("arguments")
+// document.write(confirm);
+
+// let trois = "Recherche";
+// document.write(trois);
+// alert("Site en cours de construction");
+// document.write(confirm);
+
+
