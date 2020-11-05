9fd0c2935c27af1f481241f9baf890b0eb2c2efa
Baptiste Choquet <baptiste.choquet@pm.me>
2020-11-05 11:14:40 +0100

    V4 Notation

--- Community.js
+++ Community.js
@@ -1,5 +1,19 @@
+    //Get the video
+    var video = document.getElementById("myVideo");
+    
+    // Get the button
+    var btn = document.getElementById("myBtn");
 
-
+    let elem = document.getElementById("myVideo");
+    function openFullscreen() {
+      if (elem.requestFullscreen) {
+        elem.requestFullscreen();
+      } else if (elem.webkitRequestFullscreen) { /* Safari */
+        elem.webkitRequestFullscreen();
+      } else if (elem.msRequestFullscreen) { /* IE11 */
+        elem.msRequestFullscreen();
+      }
+    }
 
 function nextFunction(){
     window.location = "lejeudeladame.html";
