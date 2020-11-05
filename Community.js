0d104d2312133a2c9327f3a598d35e9dbbd2e357
Baptiste Choquet <baptiste.choquet@pm.me>
2020-11-05 15:41:40 +0100

    V5 localstorage

--- Community.js
+++ Community.js
@@ -18,3 +18,8 @@
 function nextFunction(){
     window.location = "lejeudeladame.html";
 }
+
+let inputStar = document.getElementsByClassName("rate");
+localStorage.setItem("rate", inputStar.value);
+
+localStorage.getItem("rate");
