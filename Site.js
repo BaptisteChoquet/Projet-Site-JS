2bd6eeedca82228e38de17a731392026d6885a35
Baptiste Choquet <baptiste.choquet@pm.me>
2020-11-06 13:44:34 +0100

    V7 Commit

--- Site.js
+++ Site.js
@@ -1,4 +1,20 @@
-
-
+function passwordGood(){
+    let psw1 = document.getElementById("psw");
+    let psw2 = document.getElementById("psw-repeat");
+    let n = psw1.toString().localeCompare(psw2.toString());
+    if (psw1.length < 6) {
+    console.log("trop court")
+    window.alert("Mot de passe trop court")
+  }
+  else if (n != 0) {
+    console.log("différent")
+    window.alert("Mot de passe incorrect")
+  }
+  else {
+    console.log("bonMdP")
+    window.location= "signup.html";
+  }
+  }
+    
 
 
